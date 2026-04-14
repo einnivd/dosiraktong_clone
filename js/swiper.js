b213791b3dc3e493f244c10af09f458812c45306
einnivd <yelk0427@gmail.com>
2026-04-14 11:50:22 +0900

    상단 비주얼 스와이퍼 적용, 상단 스크롤 기능 스와이퍼 페이지네이션 기능 추가 예정

---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+window.addEventListener("load" , ()=>{
+    const swVisual = new Swiper(".sw-visual" , {
+        effect: "fade",
+        // loop: true 마지막 슬라이드 다음에 다시 첫 번째 슬라이드로 이동
+        loop: true,
+    // 슬라이드의 모션 속도를 transition 맞춘다.
+    speed: 1500,
+    autoplay: {
+      delay: 2500,
+      // 사용자가 마우스 클릭 드래그로 이동하면
+      // 아래 구문이 없으면 autoplay 가 해제되므로
+      // 이것을 방지해 주기위한 처리
+      disableOnInteraction: false,
+    },
+    })
+})
