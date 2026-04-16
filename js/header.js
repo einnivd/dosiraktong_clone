ca35128107a4fc48f0c6c33f397dfab61c9da871
einnivd <yelk0427@gmail.com>
2026-04-16 18:05:19 +0900

    모바일 화면 전환시 메뉴 버튼 변경

--- js/header.js
+++ js/header.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", () => {
   // 스크롤할 때
   const header = document.querySelector(".header");
+  const mbt = document.querySelector(".mbt")
+  const navMb = document.querySelector(".nav-mb")
   let scy = 0;
   // 1. 스크롤바의 픽셀 위치값을 파악해서
   // scy = window.document.documentElement.scrollTop;
@@ -15,8 +17,62 @@ window.addEventListener("load", () => {
     scy = window.document.documentElement.scrollTop;
     if (scy > 0) {
       header.classList.add("active");
+      mbt.classList.add("active");
     } else {
       header.classList.remove("active");
+      mbt.classList.remove("active");
     }
   });
+  // 모바일 메뉴 클릭시
+
+  // mbt.addEventListener("click" , ()=>{
+  //   navMb.classList.add("active")
+  // })
+
+  const htmlRoot = document.querySelector("html")
+  mbt.addEventListener("click" , ()=>{
+
+    // contains() 현재 ani라는 클래스가 있는지 없는지 파악
+    const state = mbt.classList.contains("ani");
+    console.log(state);
+    
+    if(state){
+      navMb.classList.remove("active");
+      mbt.classList.remove("ani");
+      // 윈도우 스크롤바가 나타남
+      htmlRoot.classList.remove("active");
+      header.classList.remove("active");
+      mbt.classList.remove("active");
+    }else{
+      mbt.classList.add("ani");
+      navMb.classList.add("active");
+      htmlRoot.classList.add("active");
+      header.classList.add("active");
+      mbt.classList.add("active");
+    }
+  })
+  // 반응형 처리
+  // 모바일 메뉴 초기화
+  // let winW = window.innerWidth;
+  // console.log(winW);
+  window.addEventListener("resize" , ()=>{
+    let winW = window.innerWidth;
+    // mobile ===> pc전환
+    if(winW > 1024){
+      mbt.classList.remove("ani")
+      navMb.classList.remove("active");
+      htmlRoot.classList.remove("active")
+      if(scy > 0){
+        // 스크롤 된 상태에서 화면 리사이징
+        header.classList.add("active");
+        mbt.classList.add("active");
+      }else{
+        header.classList.remove("active");
+         mbt.classList.remove("active");
+      }
+    }
+
+  })
+  
 });
+
